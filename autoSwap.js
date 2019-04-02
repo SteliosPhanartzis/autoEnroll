388978d816669f505d5f62bb5bacf977f2030ad1
SteliosPhanartzis <stelios.phan@gmail.com>
2019-04-02 09:51:34 -0400

    Updated autoRegister file and added autoSwap. Be careful, autoSwap is very alpha, thoroughly read the code before using

---
+++ autoSwap.js
@@ -0,0 +1,186 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+	
+
+
+/*****************************************
+ * NOTIFICATION SYSTEM, WORK IN PROGRESS
+ *****************************************/
+// var nodemailer = require('nodemailer');
+// 	//classEnroll = '';
+// var mailOptions
+// var transport = {
+//   service: 'gmail',
+//   auth: {
+//     user: '',
+//     pass: ''
+//   }
+// };
+// var transporter = nodemailer.createTransport(transport);
+// function sendMail(input){
+// 	mailOptions = {
+// 	  from: '',
+// 	  to: '',
+// 	  subject: ' Class Registered!',
+// 	  text: `You have been enrolled for ${input}`
+// 	};
+// 	transporter.sendMail(mailOptions, function(error, info){
+// 			if (error) {
+// 				console.log(error);
+// 			} else {
+// 			console.log('Email sent: ' + info.response);
+// 			}
+// 	});
+// }
+
+/*****************************************
+ * MODIFY THE FOLLOWING VARS
+ *****************************************/
+
+//Link to shopping cart for specified term
+//Current Link is Fall 2019
+var termLink = 'https://hrsa.cunyfirst.cuny.edu/psc/cnyhcprd/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES_2.SSR_SSENRL_CART.GBL?Page=SSR_SSENRL_CART&Action=A&ACAD_CAREER=UGRD&ENRL_REQUEST_ID=&INSTITUTION=QNS01&STRM=1199&TargetFrameName=None';
+var swapLink = 'https://hrsa.cunyfirst.cuny.edu/psc/cnyhcprd/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_SWAP.GBL?Page=SSR_SSENRL_SWAP&Action=A&ACAD_CAREER=UGRD&ENRL_REQUEST_ID=&INSTITUTION=QNS01&STRM=1199';
+//Test on Summer 2019
+// var termLink = 'https://hrsa.cunyfirst.cuny.edu/psc/cnyhcprd/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES_2.SSR_SSENRL_CART.GBL?Page=SSR_SSENRL_CART&Action=A&ACAD_CAREER=UGRD&ENRL_REQUEST_ID=&INSTITUTION=QNS01&STRM=1196&TargetFrameName=None';
+// var swapLink = 'https://hrsa.cunyfirst.cuny.edu/psc/cnyhcprd/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_SWAP.GBL?Page=SSR_SSENRL_SWAP&Action=A&ACAD_CAREER=UGRD&ENRL_REQUEST_ID=&INSTITUTION=QNS01&STRM=1196';
+
+//Enter First.Last## Here
+var userName = '';
+//Enter Password Here
+var pass = '';
+
+/***********************************************
+*
+* EDIT THE CLASSSWAPVAL AND CLASSDROPVAL VALUES 
+* TO THE NUMBERS OF THE CLASSES
+* ie: classSwapVal = '12345';
+* PLEASE ALSO MODIFY LINES 129 AND 157-159
+*
+***********************************************/
+//Class to drop
+var classDropVal = '';
+//Class to enroll
+var classSwapVal = '';
+
+/*****************************************
+* ONLY MODIFY NIGHTMARE INSTANCE BELOW
+******************************************/
+
+var indexRouter = require('./routes/index');
+var app = express();
+var classEnroll = 'none';
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+function classStatus (){
+const Nightmare = require('nightmare')
+//Window Settings
+//Modify x & y for positioning
+const nightmare = new Nightmare({ show: true, alwaysOnTop: false, x: 2200, y: 1200, frame: false, height: 200, width: 350, typeInterval: 20, waitTimeout: 8000});
+	setTimeout(function (){
+	nightmare
+	  .goto('https://home.cunyfirst.cuny.edu')
+	  .type('#CUNYfirstUsernameH', userName)
+	  .type('#CUNYfirstPassword', pass)
+	  .click('#submit')
+	  .wait('#ptpgltlbl_CU_CUNYFIRST_NOTICES_HMPG')
+	  .goto(termLink)
+	  .wait('#DERIVED_SSS_SCT_SSS_TERM_LINK')
+	  .evaluate(() => {
+	  		var table = document.getElementById('SSR_REGFORM_VW$scroll$0');
+	  		var classNum = document.getElementById('SSR_REGFORM_VW$scroll$0').getElementsByClassName('PSLEVEL3GRIDWBO');
+		  	/***********************************************
+			*
+			* EDIT THE CLASSSWAPVAL VALUE TO THE NUMBER OF 
+			* THE CLASS
+			* ie: classSwapVal = '12345';
+			*
+			***********************************************/
+	  		//Class to drop
+			var classSwapVal = '';
+
+	  		var temp = '';
+	  		classEnroll = 'none';
+			for (var i = 2; i < table.rows.length; i++) {
+			   var status = table.rows[i].cells[6].getElementsByTagName('img')[0].alt;
+			   var classNo = table.rows[i].cells[1].getElementsByTagName('a')[0].innerHTML;
+			   var select = table.rows[i].cells[0].getElementsByClassName('PSCHECKBOX')[0];
+			   temp = classNo.split('<br>\n ');
+			   if(status == 'Open' && temp[1].replace(/[()]/g,"") == classSwapVal){
+			   		classEnroll = [temp[0],temp[1].replace(/[()]/g,""), status];
+			   		break;
+			   }
+			}
+	  		return classEnroll; 
+		})
+	  .goto(swapLink)
+	  .wait('#DERIVED_SSS_SCT_SSS_TERM_LINK')
+	  .evaluate(() => {
+	  		var classDropList = document.getElementById('DERIVED_REGFRM1_DESCR50$225$');
+	  		var classSwapList = document.getElementById('DERIVED_REGFRM1_SSR_CLASSNAME_35$183$');
+		  	/***********************************************
+			*
+			* EDIT THE CLASSSWAPVAL AND CLASSDROPVAL VALUES 
+			* TO THE NUMBER OF THE CLASSES
+			* ie: classSwapVal = '12345';
+			*
+			***********************************************/
+			//Class to drop
+			var classDropVal = '46674';
+			//Class to enroll
+			var classSwapVal = '50531';
+
+	  		for(var i = 0; i < classDropList.options.length; i++){
+				if(classDropList.options[i].value == classDropVal)
+					classDropList.selectedIndex = i;
+			}
+			for(var i = 0; i < classSwapList.options.length; i++){
+				if(classSwapList.options[i].value == classSwapVal)
+					classSwapList.selectedIndex = i;
+			}
+			submitAction_win0(document.win0,'DERIVED_REGFRM1_SSR_PB_ADDTOLIST1$184$');
+	  })
+	  .wait('#DERIVED_REGFRM1_SSR_PB_SUBMIT')
+	  .wait(1000)
+	  .click('#DERIVED_REGFRM1_SSR_PB_SUBMIT')
+	  .wait(2000)
+	  .end()
+	  .catch(error => {
+	    console.error('Search failed:', error)
+	  });
+	  if(classEnroll == 'none')
+	  	classStatus()
+	},20000)
+}
+classStatus();
+module.exports = app;
